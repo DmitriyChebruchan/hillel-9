ae0fc17607064897ca6fbc8cd8f5bbd43a486fe0
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-10-25 00:30:01 +0300

    application order_creator is created and added to settings

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "order_creator.apps.OrderCreatorConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
